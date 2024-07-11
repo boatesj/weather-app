a056b7fae1b0ec0b5012f7c660ad38b6c5b7daf5
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2024-07-11 11:58:22 +0000

    Add conditions for event listener and error display

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,36 +5,39 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "4cee11a3a7e82d9fb8aae679f6c88b21";
 
-weatherForm.addEventListener("submit", event => {
-
+weatherForm.addEventListener("submit", async event => {
     event.preventDefault();
-
     const city = cityInput.value;
 
-    if(city) {
-
+    if (city) {
+        try {
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        } catch (error) {
+            console.error(error);
+            displayError(error.message);
+        }
     } else {
         displayError("Please enter a city");
     }
-
 });
 
 async function getWeatherData(city) {
-
+    
 }
 
 //Weather info display
 function displayWeatherInfo(data) {
-
+   
 }
 
-//Weather emojis
+//Weather emojis with colors
 function getWeatherEmoji(weatherId) {
-
+    
+    }
 }
 
 function displayError(message) {
-
     const errorDisplay = document.createElement("p");
     errorDisplay.textContent = message;
     errorDisplay.classList.add("errorDisplay");
@@ -42,5 +45,4 @@ function displayError(message) {
     card.textContent = "";
     card.style.display = "flex";
     card.appendChild(errorDisplay);
-
 }
